302e8724cf8548228ccfdd832dc2c325e8d2460c
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-02-24 16:25:47 +0800

    Update radar.js
    R7

--- radar.js
+++ radar.js
@@ -2,25 +2,20 @@ let map;
 
 function initRadar(lat, lon) {
 
-  if(map) {
-    map.remove();
+  const container = L.DomUtil.get('radarMap');
+  if(container != null){
+    container._leaflet_id = null;
   }
 
-  map = L.map('radarMap', {
-    zoomControl: false
-  }).setView([lat, lon], 7);
+  map = L.map('radarMap').setView([lat, lon], 7);
 
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap'
-  }).addTo(map);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
+    .addTo(map);
 
   L.tileLayer(
     'https://tilecache.rainviewer.com/v2/radar/latest/256/{z}/{x}/{y}/2/1_1.png',
     { opacity: 0.6 }
   ).addTo(map);
 
-  // 🔥 FIX PENTING
-  setTimeout(() => {
-    map.invalidateSize();
-  }, 300);
+  setTimeout(() => map.invalidateSize(), 500);
 }
