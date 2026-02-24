4ad6d097a9efaa0e6c999172635bbe0d20da8e93
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-02-24 16:20:15 +0800

    Update radar.js
    R5

--- radar.js
+++ radar.js
@@ -1,14 +1,19 @@
-let map;
-
 function initRadar(lat, lon) {
-  if(map) map.remove();
+
+  const container = L.DomUtil.get('radarMap');
+  if(container != null){
+    container._leaflet_id = null;
+  }
 
   map = L.map('radarMap').setView([lat, lon], 7);
 
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
     .addTo(map);
 
-  L.tileLayer('https://tilecache.rainviewer.com/v2/radar/latest/256/{z}/{x}/{y}/2/1_1.png', {
-    opacity: 0.6
-  }).addTo(map);
+  L.tileLayer(
+    'https://tilecache.rainviewer.com/v2/radar/latest/256/{z}/{x}/{y}/2/1_1.png',
+    { opacity: 0.6 }
+  ).addTo(map);
+
+  setTimeout(() => map.invalidateSize(), 500);
 }
